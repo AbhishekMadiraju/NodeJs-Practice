4e38b8ef3b160de5d792ee430cd46342e2df76fc
Abhishek Madiraju <abhishekmadiraju@abhisheks-mbp.lan>
2019-08-24 21:31:25 -0400

    Added handlebar templates

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
 const path = require('path');
 
-app.set('view engine', 'pug');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
-const adminRoutes = require('./routes/admin');
+const app = express();
+
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(400).render('404', {pageTitle: 'Page not found'});
-})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
